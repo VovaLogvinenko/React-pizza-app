d8bbecb257dd9b88664aa1ec04ce73cd3d013e8d
unknown <vovalogvinenco@gmail.com>
2022-10-26 17:46:50 +0300

    change index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { store } from './store/store'
 import { Provider } from 'react-redux';
 import App from './App';
@@ -8,9 +8,9 @@ import './firebase'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <HashRouter >
         <Provider store={store}>
             <App />
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
 );
